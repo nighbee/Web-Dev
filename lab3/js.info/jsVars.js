d85df14eac0224702356839a581af57d6472ce4f
Your Name <nightblue200416@gmail.com>
2026-02-10 10:05:29 +0500

    upd

--- lab3/js.info/jsVars.js
+++ lab3/js.info/jsVars.js
@@ -1,3 +1,5 @@
+//working with vars
+
 let admin, name 
 
 name =" John"
@@ -5,3 +7,13 @@ name =" John"
 admin = name 
 
 alert(admin)
+
+// giving the right name 
+
+const EARTH="EARTH"
+let currenUser; 
+
+
+//uppercase const 
+const BIRTHDAY = "2002"; 
+// better to use const for const vars that do not change and static 
