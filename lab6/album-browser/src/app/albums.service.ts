3b440b223c6e5146ea8189ba3581be40a4793d9f
Your Name <nightblue200416@gmail.com>
2026-03-02 22:02:26 +0500

    lab6 upd

--- lab6/album-browser/src/app/albums.service.ts
+++ lab6/album-browser/src/app/albums.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 import { Album } from './models/album.model';
 import { Photo } from './models/photo.model';
 
@@ -9,14 +10,24 @@ import { Photo } from './models/photo.model';
 })
 export class AlbumsService {
   private apiUrl = 'https://jsonplaceholder.typicode.com/albums';
+  private albumsCache: Album[] | null = null;
 
   constructor(private http: HttpClient) { }
 
   getAlbums(): Observable<Album[]> {
-    return this.http.get<Album[]>(this.apiUrl);
+    if (this.albumsCache) {
+      return of(this.albumsCache);
+    }
+    return this.http.get<Album[]>(this.apiUrl).pipe(
+      tap(albums => this.albumsCache = albums)
+    );
   }
 
   getAlbum(id: number): Observable<Album> {
+    if (this.albumsCache) {
+      const album = this.albumsCache.find(a => a.id === id);
+      if (album) return of(album);
+    }
     return this.http.get<Album>(`${this.apiUrl}/${id}`);
   }
 
@@ -25,10 +36,41 @@ export class AlbumsService {
   }
 
   updateAlbum(album: Album): Observable<Album> {
-    return this.http.put<Album>(`${this.apiUrl}/${album.id}`, album);
+    return this.http.put<Album>(`${this.apiUrl}/${album.id}`, album).pipe(
+      tap(updatedAlbum => {
+        if (this.albumsCache) {
+          const index = this.albumsCache.findIndex(a => a.id === album.id);
+          if (index !== -1) {
+            this.albumsCache[index] = updatedAlbum;
+          }
+        }
+      }),
+      catchError((err: any) => {
+        // Fallback update for dummy API
+        if (this.albumsCache) {
+          const index = this.albumsCache.findIndex(a => a.id === album.id);
+          if (index !== -1) {
+            this.albumsCache[index] = album;
+          }
+        }
+        return throwError(() => err);
+      })
+    );
   }
 
   deleteAlbum(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
+      tap(() => {
+        if (this.albumsCache) {
+          this.albumsCache = this.albumsCache.filter(a => a.id !== id);
+        }
+      }),
+      catchError(err => {
+        if (this.albumsCache) {
+          this.albumsCache = this.albumsCache.filter(a => a.id !== id);
+        }
+        return throwError(() => err);
+      })
+    );
   }
 }
