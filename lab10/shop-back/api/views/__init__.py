fbd9919354708b57a173a6eb26051d353f1e1147
Your Name <nightblue200416@gmail.com>
2026-04-07 10:03:48 +0500

    upd

--- lab10/shop-back/api/views/__init__.py
+++ lab10/shop-back/api/views/__init__.py
@@ -1,4 +1,5 @@
-# Switch between implementations here
+# Switch between implementations: lvl5 generic, lvl4 mixin, 
+# lvl3 class based, lvl2 Function based 
 
 # from .fbv import product_list, product_detail
 # from .cbv import ProductListAPIView, ProductDetailAPIView
